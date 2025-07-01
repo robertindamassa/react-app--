eb6e27335f06400bbbf3cb227b73378cbdfc146b
robertindamassa <roberto_paula-fil@estudante.sesisenai.org.br>
2025-07-01 16:02:27 -0300

    Update HomeScreen.tsx

--- ativid-react/src/assets/screens/HomeScreen.tsx
+++ ativid-react/src/assets/screens/HomeScreen.tsx
@@ -12,3 +12,24 @@ export default function HomeScreen() {
     setTarefa(""); 
   };
 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>TAREFAS</Text>
+      <Image
+        source={{ uri: "https://cdn-icons-png.flaticon.com/512/1582/1582717.png" }}
+        style={styles.logo}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite uma tarefa"
+        placeholderTextColor="#888"
+        value={tarefa}
+        onChangeText={setTarefa}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Enviar" onPress={enviarTarefa} color="#2D2DFF" />
+      </View>
+    </View>
+  );
+}
+
