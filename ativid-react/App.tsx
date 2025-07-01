958ed786e23383661fee5f9e02ea7cafa9bf3f24
robertindamassa <roberto_paula-fil@estudante.sesisenai.org.br>
2025-07-01 16:42:33 -0300

    corzinha
    botando cor

--- ativid-react/App.tsx
+++ ativid-react/App.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start", 
     alignItems: "center",
-    backgroundColor: "#F3F3FF",
+    backgroundColor: "#FFDEAD",
   },
   logo: {
     width: 200,
