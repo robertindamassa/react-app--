d34fe68a0401593f6a8293cc90d5e5c2c83b354a
robertindamassa <roberto_paula-fil@estudante.sesisenai.org.br>
2025-07-01 16:00:18 -0300

    Update HomeScreen.tsx

--- ativid-react/src/assets/screens/HomeScreen.tsx
+++ ativid-react/src/assets/screens/HomeScreen.tsx
@@ -2,3 +2,7 @@ import { useState } from "react";
 import { View, Text, StyleSheet, Image, TextInput, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+export default function HomeScreen() {
+  const [tarefa, setTarefa] = useState("");
+  const navigation = useNavigation();
+
